1500e13ff7098de7940fa1b4bf66777d9abfa7d5
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2015-02-17 13:34:12 +0100

    Add Grunt ESLint plugin

---
+++ grunt/eslint.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+	/**
+	* Check the quality of our JS files
+	*/
+	module.exports = {
+		app: ['angular-logs2server.js'],
+		options: {
+			config: 'grunt/eslint.json'
+		}
+	};
+})();
